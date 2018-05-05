fc483b58075f76a2b7ba74d8071635d195d71263
Daniel Winther Jensen <danielwinther@hotmail.dk>
2018-05-05 18:50:11 +0200

    Refactoring

---
+++ src/js/custom.js
@@ -0,0 +1,9 @@
+/*var existInterval = setInterval(function() {
+    var subtitles = $('.subtitle-cue p');
+
+    if (subtitles.length) {
+        console.log('exists!');
+        subtitles.addClass('vs-background');
+        clearInterval(existInterval);
+    }
+}, 100);*/
