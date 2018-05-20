0b2e93fcd49ffd154d717f5a514ad015ad1f00db
Daniel Winther Jensen <danielwinther@hotmail.dk>
2018-05-20 09:28:47 +0200

    Converted to vanilla JavaScript

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(function(details) {
 	if(details.reason == 'install'){
-		chrome.storage.sync.set({'spectrum_background': 'black', 'spectrum_text': 'white'});
+		chrome.storage.sync.set({'spectrum_background': '#000000', 'spectrum_text': '#ffffff'});
 	}
 });
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,35 +1,38 @@
 chrome.storage.sync.get(['spectrum_background', 'spectrum_text'], function(storage) {
-    startInterval(10);
+    var doc = document;
+    var subtitles = setPopupColors(doc.querySelector('.example-subtitle-cue p'));
 
-    var subtitles = setCurrentColors($('.example-subtitle-cue p'));
-    initializeColorPicker($('#background'), storage.spectrum_background, 'spectrum_background');
-    initializeColorPicker($('#text'), storage.spectrum_text, 'spectrum_text');
+    startInterval(10);
+    initializeColorPicker(doc.querySelector('#background'), storage.spectrum_background, 'spectrum_background');
+    initializeColorPicker(doc.querySelector('#text'), storage.spectrum_text, 'spectrum_text');
+
 
     function startInterval(milliseconds) {
         setInterval(function() {
-            if ($('.subtitle-cue p').length) {
-                $('.subtitle-cue p').css('cssText', 'background: ' + storage.spectrum_background + ' !important; color: ' + storage.spectrum_text + ' !important;');
+            viaplaySubtitles = doc.querySelector('.subtitle-cue p');
+            if (viaplaySubtitles) {
+                viaplaySubtitles.style.cssText = 'background: ' + storage.spectrum_background + ' !important; color: ' + storage.spectrum_text + ' !important;';
             }
         }, milliseconds);
     }
-    function setCurrentColors(element) {
-        return element.css('cssText', 'background: ' + storage.spectrum_background + ' !important; color: ' + storage.spectrum_text + ' !important;');
+    function setPopupColors(element) {
+        if (element) {
+            element.style.cssText = 'background: ' + storage.spectrum_background + ' !important; color: ' + storage.spectrum_text + ' !important;';
+            return element;
+        }
     }
     function initializeColorPicker(colorPicker, storage, setValue) {
-        colorPicker.spectrum({
-            flat: true,
+        new Picker({
+            parent: colorPicker,
+            popup: 'bottom',
             color: storage,
-            preferredFormat: 'rgb',
-            showInput: true,
-            showAlpha: true,
-            maxSelectionSize: 1,
-            change: function(color) {
+            onDone: function(color) {
                 var object = {};
-                object[setValue] = color.toRgbString();
+                object[setValue] = color.hex;
 
                 chrome.storage.sync.set(object, function() {
                     chrome.storage.sync.get(['spectrum_background', 'spectrum_text'], function(storageChange) {
-                        subtitles.css('cssText', 'background: ' + storageChange.spectrum_background + ' !important; color: ' + storageChange.spectrum_text + ' !important;');
+                        subtitles.style.cssText = 'background: ' + storageChange.spectrum_background + ' !important; color: ' + storageChange.spectrum_text + ' !important;';
                     });
                 });
             }
