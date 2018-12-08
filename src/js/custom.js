7dc27748c608b2554bc849488b89ea2b900c0875
Daniel <danielwinther@hotmail.dk>
2018-12-08 17:47:49 +0100

    Added movie quotes and other minor details

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,5 +1,19 @@
 chrome.storage.sync.get(['spectrum_background', 'spectrum_text'], function(storage) {
     var doc = document;
+    var movieQuotes = [
+    "I'm going to make him an offer he can't refuse.",
+    "May the Force be with you.",
+    "Mama always said life was like a box of chocolates. You never know what you're gonna get.",
+    "The first rule of Fight Club is you do not talk about Fight Club.",
+    "Houston, we have a problem.",
+    "Say hello to my little friend!",
+    "Snakes. Why'd it have to be snakes?",
+    "I see dead people.",
+    "Elementary, my dear Watson.",
+    "To infinity and beyond!"
+    ];
+
+    setMovieQuote(doc.querySelector('.example-subtitle-cue p'), movieQuotes);
     var subtitles = setPopupColors(doc.querySelector('.example-subtitle-cue p'));
 
     startInterval(10);
@@ -7,6 +21,18 @@ chrome.storage.sync.get(['spectrum_background', 'spectrum_text'], function(stora
     initializeColorPicker(doc.querySelector('#text'), storage.spectrum_text, 'spectrum_text');
 
 
+    function setMovieQuote(element, movieQuotes) {
+        if (element) {
+            var randomQuote = movieQuotes[Math.floor(Math.random() * movieQuotes.length)];
+            element.textContent = randomQuote;
+        }
+    }
+    function setPopupColors(element) {
+        if (element) {
+            element.style.cssText = 'background: ' + storage.spectrum_background + ' !important; color: ' + storage.spectrum_text + ' !important;';
+            return element;
+        }
+    }
     function startInterval(milliseconds) {
         setInterval(function() {
             viaplaySubtitles = doc.querySelector('.subtitle-cue p');
@@ -14,12 +40,6 @@ chrome.storage.sync.get(['spectrum_background', 'spectrum_text'], function(stora
                 viaplaySubtitles.style.cssText = 'background: ' + storage.spectrum_background + ' !important; color: ' + storage.spectrum_text + ' !important;';
             }
         }, milliseconds);
-    }
-    function setPopupColors(element) {
-        if (element) {
-            element.style.cssText = 'background: ' + storage.spectrum_background + ' !important; color: ' + storage.spectrum_text + ' !important;';
-            return element;
-        }
     }
     function initializeColorPicker(colorPicker, storage, setValue) {
         new Picker({
@@ -29,12 +49,13 @@ chrome.storage.sync.get(['spectrum_background', 'spectrum_text'], function(stora
             onDone: function(color) {
                 var object = {};
                 object[setValue] = color.hex;
-
                 chrome.storage.sync.set(object, function() {
                     chrome.storage.sync.get(['spectrum_background', 'spectrum_text'], function(storageChange) {
                         subtitles.style.cssText = 'background: ' + storageChange.spectrum_background + ' !important; color: ' + storageChange.spectrum_text + ' !important;';
                     });
                 });
+                console.log('%c ' + color.hex +' ', 'color: ' + color.hex);
+                doc.querySelector('#alert').classList.remove('d-none');
             }
         });
     }
