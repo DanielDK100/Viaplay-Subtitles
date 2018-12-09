b007f44fd10c2dbf214f4c35ebd4f6d1428bbba6
Daniel <danielwinther@hotmail.dk>
2018-12-09 15:50:23 +0100

    Hotloading when chosing colors

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(function(details) {
-	if(details.reason == 'install'){
+	if(details.reason == 'install') {
 		chrome.storage.sync.set({'vs_background': '#000000', 'vs_text': '#ffffff'});
 	}
 });
