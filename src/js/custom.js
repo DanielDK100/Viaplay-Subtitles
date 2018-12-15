7fac3d8234925228299d0b06df86f029c6623ae7
danielwinther <danielwinther@hotmail.dk>
2018-12-15 16:33:15 +0100

    Added message API

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,28 +1,42 @@
-chrome.storage.sync.get(['vs_background', 'vs_text'], function(storage) {
-    var manifest = chrome.runtime.getManifest();
-    var doc = document;
-    var movieQuotes = [
-    "I'm going to make him an offer he can't refuse.",
-    "May the Force be with you.",
-    "Mama always said life was like a box of chocolates. You never know what you're gonna get.",
-    "The first rule of Fight Club is you do not talk about Fight Club.",
-    "Houston, we have a problem.",
-    "Say hello to my little friend!",
-    "Snakes. Why'd it have to be snakes?",
-    "I see dead people.",
-    "Elementary, my dear Watson.",
-    "To infinity and beyond!"
-    ];
+var manifest = chrome.runtime.getManifest();
+var doc = document;
+var movieQuotes = [
+"I'm going to make him an offer he can't refuse.",
+"May the Force be with you.",
+"Mama always said life was like a box of chocolates. You never know what you're gonna get.",
+"The first rule of Fight Club is you do not talk about Fight Club.",
+"Houston, we have a problem.",
+"Say hello to my little friend!",
+"Snakes. Why'd it have to be snakes?",
+"I see dead people.",
+"Elementary, my dear Watson.",
+"To infinity and beyond!"
+];
+var vsBackground = 'vs_background';
+var vsText = 'vs_text';
+var vsStyle = 'vs-style';
+
+chrome.storage.sync.get([vsBackground, vsText], function(storage) {
+    initializeMessageListener(vsBackground, vsText, vsStyle);
 
     setManifestData(manifest);
     setMovieQuote(doc.querySelector('.example-subtitle-cue p'), movieQuotes);
-    var subtitles = setPopupColors(doc.querySelector('.example-subtitle-cue p'), storage);
+    setPopupColors(vsStyle, storage);
 
-    initializeColorPicker(doc.querySelector('#background'), storage.vs_background, 'vs_background');
-    initializeColorPicker(doc.querySelector('#text'), storage.vs_text, 'vs_text');
+    initializeColorPicker(doc.querySelector('#background'), storage.vs_background, vsBackground);
+    initializeColorPicker(doc.querySelector('#text'), storage.vs_text, vsText);
 
-    startInterval('.subtitle-cue p', 10);
-
+    function initializeMessageListener(vsBackground, vsText, vsStyle) {
+        chrome.runtime.onMessage.addListener(
+            function(request, sender, sendResponse) {
+                if (request.color) {
+                    chrome.storage.sync.get([vsBackground, vsText], function(storageChange) {
+                        doc.querySelector('#' + vsStyle).innerHTML = setStyles(storageChange);
+                    });
+                    logColor(request.color);
+                }
+            });
+    }
     function setManifestData(manifest) {
         if (doc.querySelector('#vs-container')) {
             var titles = doc.querySelectorAll('.vs-title');
@@ -40,10 +54,14 @@ chrome.storage.sync.get(['vs_background', 'vs_text'], function(storage) {
             element.textContent = randomQuote;
         }
     }
-    function setPopupColors(element, storage) {
-        if (element) {
-            element.style.cssText = 'background: ' + storage.vs_background + ' !important; color: ' + storage.vs_text + ' !important;';
-            return element;
+    function setPopupColors(id, storage) {
+        var head = doc.head
+        if (head) {
+            var style = doc.createElement('style');
+            style.type = 'text/css';
+            style.id = id;
+            style.appendChild(doc.createTextNode(setStyles(storage)));
+            head.appendChild(style);
         }
     }
     function initializeColorPicker(colorPicker, storage, setValue) {
@@ -55,22 +73,25 @@ chrome.storage.sync.get(['vs_background', 'vs_text'], function(storage) {
                 var object = {};
                 object[setValue] = color.hex;
                 chrome.storage.sync.set(object, function() {
-                    chrome.storage.sync.get(['vs_background', 'vs_text'], function(storageChange) {
-                        subtitles.style.cssText = 'background: ' + storageChange.vs_background + ' !important; color: ' + storageChange.vs_text + ' !important;';
+                    chrome.storage.sync.get([vsBackground, vsText], function(storageChange) {
+                        doc.querySelector('#' + vsStyle).innerHTML = setStyles(storageChange);
+                        chrome.tabs.query({url: '*://*/*'}, function(tabs) {
+                            if (tabs) {
+                                tabs.forEach(function(tab) {
+                                    chrome.tabs.sendMessage(tab.id, {color: color.hex});
+                                });
+                            }
+                        });
                     });
                 });
-                console.log('%c ' + color.hex +' ', 'color: ' + color.hex);
+                logColor(color.hex);
             }
         });
     }
-    function startInterval(element, milliseconds) {
-        setInterval(function() {
-            viaplaySubtitles = doc.querySelector(element);
-            if (viaplaySubtitles) {
-                chrome.storage.sync.get(['vs_background', 'vs_text'], function(storage) {
-                    viaplaySubtitles.style.cssText = 'background: ' + storage.vs_background + ' !important; color: ' + storage.vs_text + ' !important;';
-                });
-            }
-        }, milliseconds);
+    function logColor(hexColor) {
+        console.log('%c ' + hexColor +' ', 'color: ' + hexColor);
+    }
+    function setStyles(storage) {
+        return '.subtitle-cue p, .example-subtitle-cue p {background: ' + storage.vs_background + ' !important; color: ' + storage.vs_text + ' !important;}';
     }
 });
