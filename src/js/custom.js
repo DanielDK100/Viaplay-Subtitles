94bee78594609d53901025f975acda790abba205
danielwinther <danielwinther@hotmail.dk>
2018-12-15 18:45:52 +0100

    Moved functions out of storage callback

--- src/js/custom.js
+++ src/js/custom.js
@@ -25,73 +25,73 @@ chrome.storage.sync.get([vsBackground, vsText], function(storage) {
 
     initializeColorPicker(doc.querySelector('#background'), storage.vs_background, vsBackground);
     initializeColorPicker(doc.querySelector('#text'), storage.vs_text, vsText);
+});
 
-    function initializeMessageListener(vsBackground, vsText, vsStyle) {
-        chrome.runtime.onMessage.addListener(
-            function(request, sender, sendResponse) {
-                if (request.color) {
-                    chrome.storage.sync.get([vsBackground, vsText], function(storageChange) {
-                        doc.querySelector('#' + vsStyle).innerHTML = setStyles(storageChange);
-                    });
-                    logColor(request.color);
-                }
-            });
-    }
-    function setManifestData(manifest) {
-        if (doc.querySelector('#vs-container')) {
-            var titles = doc.querySelectorAll('.vs-title');
-            titles.forEach(function(title) {
-                title.textContent = manifest.name;
-            });
-            doc.querySelector('.vs-icon').src = manifest.icons['128'];
-            doc.querySelector('.vs-icon').alt = manifest.name;
-            doc.querySelector('.vs-version').textContent = manifest.version;
-        }
-    }
-    function setMovieQuote(element, movieQuotes) {
-        if (element) {
-            var randomQuote = movieQuotes[Math.floor(Math.random() * movieQuotes.length)];
-            element.textContent = randomQuote;
-        }
-    }
-    function setPopupColors(id, storage) {
-        var head = doc.head
-        if (head) {
-            var style = doc.createElement('style');
-            style.type = 'text/css';
-            style.id = id;
-            style.appendChild(doc.createTextNode(setStyles(storage)));
-            head.appendChild(style);
-        }
-    }
-    function initializeColorPicker(colorPicker, storage, setValue) {
-        new Picker({
-            parent: colorPicker,
-            popup: 'bottom',
-            color: storage,
-            onDone: function(color) {
-                var object = {};
-                object[setValue] = color.hex;
-                chrome.storage.sync.set(object, function() {
-                    chrome.storage.sync.get([vsBackground, vsText], function(storageChange) {
-                        doc.querySelector('#' + vsStyle).innerHTML = setStyles(storageChange);
-                        chrome.tabs.query({url: '*://*/*'}, function(tabs) {
-                            if (tabs) {
-                                tabs.forEach(function(tab) {
-                                    chrome.tabs.sendMessage(tab.id, {color: color.hex});
-                                });
-                            }
-                        });
-                    });
+function initializeMessageListener(vsBackground, vsText, vsStyle) {
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+            if (request.color) {
+                chrome.storage.sync.get([vsBackground, vsText], function(storageChange) {
+                    doc.querySelector('#' + vsStyle).innerHTML = setStyles(storageChange);
                 });
-                logColor(color.hex);
+                logColor(request.color);
             }
         });
+}
+function setManifestData(manifest) {
+    if (doc.querySelector('#vs-container')) {
+        var titles = doc.querySelectorAll('.vs-title');
+        titles.forEach(function(title) {
+            title.textContent = manifest.name;
+        });
+        doc.querySelector('.vs-icon').src = manifest.icons['128'];
+        doc.querySelector('.vs-icon').alt = manifest.name;
+        doc.querySelector('.vs-version').textContent = manifest.version;
     }
-    function logColor(hexColor) {
-        console.log('%c ' + hexColor +' ', 'color: ' + hexColor);
+}
+function setMovieQuote(element, movieQuotes) {
+    if (element) {
+        var randomQuote = movieQuotes[Math.floor(Math.random() * movieQuotes.length)];
+        element.textContent = randomQuote;
     }
-    function setStyles(storage) {
-        return '.subtitle-cue p, .example-subtitle-cue p {background: ' + storage.vs_background + ' !important; color: ' + storage.vs_text + ' !important;}';
+}
+function setPopupColors(id, storage) {
+    var head = doc.head
+    if (head) {
+        var style = doc.createElement('style');
+        style.type = 'text/css';
+        style.id = id;
+        style.appendChild(doc.createTextNode(setStyles(storage)));
+        head.appendChild(style);
     }
-});
+}
+function initializeColorPicker(colorPicker, storage, setValue) {
+    new Picker({
+        parent: colorPicker,
+        popup: 'bottom',
+        color: storage,
+        onDone: function(color) {
+            var object = {};
+            object[setValue] = color.hex;
+            chrome.storage.sync.set(object, function() {
+                chrome.storage.sync.get([vsBackground, vsText], function(storageChange) {
+                    doc.querySelector('#' + vsStyle).innerHTML = setStyles(storageChange);
+                    chrome.tabs.query({url: '*://*/*'}, function(tabs) {
+                        if (tabs) {
+                            tabs.forEach(function(tab) {
+                                chrome.tabs.sendMessage(tab.id, {color: color.hex});
+                            });
+                        }
+                    });
+                });
+            });
+            logColor(color.hex);
+        }
+    });
+}
+function logColor(hexColor) {
+    console.log('%c ' + hexColor +' ', 'color: ' + hexColor);
+}
+function setStyles(storage) {
+    return '.subtitle-cue p, .example-subtitle-cue p {background: ' + storage.vs_background + ' !important; color: ' + storage.vs_text + ' !important;}';
+}
