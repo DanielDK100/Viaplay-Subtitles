91d65d769b03f7f05220640a8be65355e613d886
danielwinther <danielwinther@hotmail.dk>
2018-12-16 02:39:33 +0100

    Refactor background page to comply with OOP

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,31 @@
-chrome.runtime.onInstalled.addListener(function(details) {
-	if(details.reason == 'install') {
-		chrome.storage.sync.set({'vs_background': '#000000', 'vs_text': '#ffffff'});
+const ViaplaySubtitlesBackground = (function() {
+	const _vsBackground = Symbol();
+	const _background = Symbol();
+	const _vsText = Symbol();
+	const _text = Symbol();
+
+	class ViaplaySubtitlesBackground {
+		constructor(vsBackground, background, vsText, text) {
+			this[_vsBackground] = vsBackground;
+			this[_background] = background;
+			this[_vsText] = vsText;
+			this[_text] = text;
+
+			this.setDefaultColors(this[_vsBackground], this[_background], this[_vsText], this[_text]);
+		}
+
+		setDefaultColors(vsBackground, background, vsText, text) {
+			chrome.runtime.onInstalled.addListener(function(details) {
+				if(details.reason == 'install') {
+					const object = {};
+					object[vsBackground] = background;
+					object[vsText] = text;
+					chrome.storage.sync.set(object);
+				}
+			});
+		}
 	}
-});
+	return ViaplaySubtitlesBackground;
+}());
+
+new ViaplaySubtitlesBackground('vs_background', '#000000', 'vs_text', '#ffffff');
